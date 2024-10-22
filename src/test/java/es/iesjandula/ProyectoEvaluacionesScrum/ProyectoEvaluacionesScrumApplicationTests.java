ef8686314cbded00159e5997679efc3397383fa5
Agencia <jararui1003@g.educaand.es>
2024-10-22 18:44:40 +0200

    Primer commit

---
+++ src/test/java/es/iesjandula/ProyectoEvaluacionesScrum/ProyectoEvaluacionesScrumApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesjandula.ProyectoEvaluacionesScrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEvaluacionesScrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
