7c632102c21dd5add536130d7997605788ca5fa0
Juanma23255 <jgarflo1705@g.educaand.es>
2024-10-25 17:10:30 +0200

    003 Crear Entidad Alumno: He creado la entidad Alumno correctamente.

--- src/main/java/es/iesjandula/ProyectoEvaluacionesScrum/Model/Alumno.java
+++ src/main/java/es/iesjandula/ProyectoEvaluacionesScrum/Model/Alumno.java
@@ -6,6 +6,34 @@ import jakarta.persistence.Id;
 @Entity
 public class Alumno {
     @Id
+    private String email;
+
+    private String nombre;
+
+    public Alumno() {
+
+    }
+
+    public Alumno(String email, String nombre) {
+        this.email = email;
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
 
 }
