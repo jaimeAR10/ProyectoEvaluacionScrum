d7ca941f93bbcaf4d9d6c1e012ee0642f3c8b02b
Agencia <jararui1003@g.educaand.es>
2024-10-25 17:12:51 +0200

    004 Crear Entidad Evaluación: He creado la entidad evaluación.
    # Conflicts:
    #	src/main/java/es/iesjandula/ProyectoEvaluacionesScrum/ProyectoEvaluacionesScrumApplication.java

---
+++ src/main/java/es/iesjandula/ProyectoEvaluacionesScrum/Model/Evaluacion.java
@@ -0,0 +1,64 @@
+package es.iesjandula.ProyectoEvaluacionesScrum.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Evaluacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private LocalDate fecha;
+
+
+
+
+    public Evaluacion(Long id, String nombre, LocalDate fecha) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fecha = fecha;
+    }
+    public Evaluacion() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Evaluacion{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", fecha=" + fecha +
+                '}';
+    }
+}
