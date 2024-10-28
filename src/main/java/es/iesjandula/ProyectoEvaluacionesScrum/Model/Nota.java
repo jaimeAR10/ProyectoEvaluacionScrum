70d0ba34dc8f1a22e18bc140e0f1d313821beb6c
Agencia <jararui1003@g.educaand.es>
2024-10-28 16:50:32 +0100

    005 Crear entidad nota: Estoy trabajando en la entidad nota

--- src/main/java/es/iesjandula/ProyectoEvaluacionesScrum/Model/Nota.java
+++ src/main/java/es/iesjandula/ProyectoEvaluacionesScrum/Model/Nota.java
@@ -1,62 +1,91 @@
 package es.iesjandula.ProyectoEvaluacionesScrum.Model;
 
-import jakarta.persistence.EmbeddedId;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 
-public class Nota implements Serializable {
+@Entity
+public class Nota {
+
     @EmbeddedId
-    private Alumno evaluado;
+    private NotaId notaId;
+
+    @ElementCollection
+    private List<Integer> respuestas;
+
+    //relaciones
+    @ManyToOne
+    @MapsId("evaluacionId")
+    @JoinColumn(name = "evaluacion_id")
+    @JsonManagedReference
     private Evaluacion evaluacion;
-    private Alumno evaluador;
 
-    public Nota(Alumno evaluado, Evaluacion evaluacion, Alumno evaluador) {
-        this.evaluado = evaluado;
-        this.evaluacion = evaluacion;
-        this.evaluador = evaluador;
-    }
+    @ManyToMany
+    @MapsId("evaluador")
+    @JoinTable(name = "nota_alumno",
+            joinColumns = @JoinColumn(name = "nota_id"),
+            inverseJoinColumns = @JoinColumn(name = "alumno_id")
+    )
+    private List<Alumno> evaluadores;
 
+    @ManyToMany
+    @MapsId("evaluado")
+    @JoinTable(name = "nota_alumno",
+            joinColumns = @JoinColumn(name = "nota_id"),
+            inverseJoinColumns = @JoinColumn(name = "alumno_id")
+    )
+    private List<Alumno> evaluados;
+
+    //contructores
     public Nota() {}
 
-    public Alumno getEvaluado() {
-        return evaluado;
+    public Nota(NotaId notaId, List<Integer> respuestas) {
+        this.notaId = notaId;
+        this.respuestas = respuestas;
     }
 
-    public void setEvaluado(Alumno evaluado) {
-        this.evaluado = evaluado;
+    // Getters and setters
+    public NotaId getNotaId() {
+        return notaId;
+    }
+
+    public void setNotaId(NotaId notaId) {
+        this.notaId = notaId;
+    }
+
+    public List<Integer> getRespuestas() {
+        return respuestas;
+    }
+
+    public void setRespuestas(List<Integer> respuestas) {
+        this.respuestas = respuestas;
     }
 
     public Evaluacion getEvaluacion() {
         return evaluacion;
     }
-
     public void setEvaluacion(Evaluacion evaluacion) {
         this.evaluacion = evaluacion;
     }
+//getters y setters relaciones
 
-    public Alumno getEvaluador() {
-        return evaluador;
+    public List<Alumno> getEvaluadores() {
+        return evaluadores;
     }
 
-    public void setEvaluador(Alumno evaluador) {
-        this.evaluador = evaluador;
+    public void setEvaluadores(List<Alumno> evaluadores) {
+        this.evaluadores = evaluadores;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Nota nota = (Nota) o;
-        return Objects.equals(evaluado, nota.evaluado) &&
-                Objects.equals(evaluacion, nota.evaluacion) &&
-                Objects.equals(evaluador, nota.evaluador);
-
+    public List<Alumno> getEvaluado() {
+        return evaluados;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(evaluado, evaluacion, evaluador);
+    public void setEvaluado(List<Alumno> evaluados) {
+        this.evaluados = evaluados;
     }
 }
